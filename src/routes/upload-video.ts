1f328ee71dab96416a89982d25d41ae306ab5416
Edpow <e_dg_roch@hotmail.com>
2023-09-12 23:24:30 -0400

    initial commit

---
+++ src/routes/upload-video.ts
@@ -0,0 +1,31 @@
+import { FastifyInstance } from "fastify"
+import { prisma } from "../lib/prisma"
+import { fastifyMultipart } from "@fastify/multipart"
+import path from "node:path"
+import { randomUUID } from "node:crypto"
+
+export async function uploadVideoRoute(app: FastifyInstance) {
+  app.register(fastifyMultipart, {
+    limits: {
+      fileSize: 1_045_576 * 25, //25mb
+    },
+  })
+  app.post("/videos", async (request, reply) => {
+    const data = await request.file()
+    if (!data) {
+      return reply.status(400).send({ error: "MIssing file input." })
+    }
+
+    const extension = path.extname(data.filename)
+
+    if (extension != ".mp3") {
+      return reply
+        .status(400)
+        .send({ error: "Invalid input type, please upload a MP3" })
+    }
+
+    const fileNameBase = path.basename(data.filename, extension)
+    const fileUploadName = `${fileNameBase}-${randomUUID()}${extension}`
+    const uploadDir = path.resolve(__dirname, "../../tmp", fileUploadName)
+  })
+}
